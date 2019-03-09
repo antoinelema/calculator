b99f83355b0aff71779641f8fc4e5e3193030188
Unknown <antoinelemariey@gmail.com>
2019-03-09 18:06:11 +0100

    Ajout de "l'ecran" de la calculatrice qui se rempli quand on clique sur les boutons

--- calculatorInterface/calculator_interface/src/components/calculator/Calculator.js
+++ calculatorInterface/calculator_interface/src/components/calculator/Calculator.js
@@ -7,6 +7,8 @@ export default (props) => {
   Component Calculator
   affiche la calculatrice
   */
+  const [signe, setSigne] = useState([])
+  const [ans, setAns] = useState([])
   let nb_array = Array.apply(null, {length: 10}).map(Number.call, Number).reverse()
   nb_array.push(".","c")
   const symbole = ['/','*','+','-']
@@ -14,7 +16,9 @@ export default (props) => {
   return (
     <div>
       <Col xs={{size:2, offset:1}}>
-
+        <Col className="noPadding">
+          <Input value={signe.join("")} disabled block/>
+        </Col>
           <Row className="noMargin">
             <Col >
               <Row className="nb">
@@ -24,38 +28,44 @@ export default (props) => {
                     <React.Fragment>
                     {nb != "c"?(
                       <Col xs="4" className="noPadding">
-                        <Button color="primary" value={nb} key={nb} btnSize="lg" outline="true"  onClick={()=>{console.log(nb)}}/>
+                        <Button color="primary" value={nb} key={nb} btnSize="lg" outline="true"  onClick={()=>setSigne([
+                          ...signe,nb
+                        ])}/>
                       </Col>
                     ):(
                       <Col xs="4" className="noPadding">
-                        <Button color="danger" value={nb} key={nb} btnSize="lg" onClick={()=>{console.log(nb)}}/>
+                        <Button color="danger" value={nb} key={nb} btnSize="lg" onClick={()=>setSigne([
+                          ""
+                        ])}/>
                       </Col>
                     )}
                     </React.Fragment>
                     )
                   })
                 }
-                </Row>
-              </Col>
-                <Row className="noMargin">
-                  <ButtonGroup vertical>
-                    {symbole.map(sym=>{
-                      return(
-                        <Button color="dark" value={sym} key={sym} btnSize="lg" onClick={()=>{console.log(sym)}}/>
-                      )
-                    })
-                    }
-                  </ButtonGroup>
               </Row>
+            </Col>
+              <Row className="noMargin">
+                <ButtonGroup vertical>
+                  {symbole.map(sym=>{
+                    return(
+                      <Button color="dark" value={sym} key={sym} btnSize="lg" onClick={()=>setSigne([
+                        ...signe,sym
+                      ])}/>
+                    )
+                  })
+                  }
+                </ButtonGroup>
             </Row>
-            <Row className="noMargin">
-              <Col className="noPadding">
-                  <Button color="dark" value="Ans"  btnSize="lg" onClick={()=>{console.log("ans")}}/>
-              </Col>
-              <Col className="noPadding">
-                  <Button color="primary" value="="  btnSize="lg" onClick={()=>{console.log("equal")}}/>
-              </Col>
-            </Row>
+          </Row>
+          <Row className="noMargin">
+            <Col className="noPadding">
+                <Button color="dark" value="Ans"  btnSize="lg" onClick={()=>{console.log("ans")}}/>
+            </Col>
+            <Col className="noPadding">
+                <Button color="primary" value="="  btnSize="lg" onClick={()=>{console.log("equal")}}/>
+            </Col>
+          </Row>
         </Col>
     </div>
   )
