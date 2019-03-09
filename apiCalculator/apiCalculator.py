6270d85283c0298e63b50bb0461711f37db1ff40
Unknown <antoinelemariey@gmail.com>
2019-03-09 18:21:06 +0100

    Connexion et communication avec  api de calcul

--- apiCalculator/apiCalculator.py
+++ apiCalculator/apiCalculator.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
 from models.models import calculator, getTxtSaved
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
--- calculatorInterface/calculator_interface/src/components/calculator/Calculator.js
+++ calculatorInterface/calculator_interface/src/components/calculator/Calculator.js
@@ -1,22 +1,63 @@
 import React, { useState, useEffect } from 'react'
 import Button from './Button'
 import { ButtonGroup,Row, Col,Container,Input } from 'reactstrap'
+import axios from 'axios'
 
 export default (props) => {
   /*
   Component Calculator
   affiche la calculatrice
   */
+  const [lastCalc, setLastCalc] = useState([])
   const [signe, setSigne] = useState([])
   const [ans, setAns] = useState([])
   let nb_array = Array.apply(null, {length: 10}).map(Number.call, Number).reverse()
   nb_array.push(".","c")
   const symbole = ['/','*','+','-']
 
+  const equalClick =(e)=>{
+    // formate le string à envoyer
+    const strToSend=signe.join("")
+      const dataResp= apiCalcul(strToSend)
+  }
+
+  const ansClick =(e)=>{
+    //ajoute le dernier resultat à l'ecran
+    setSigne([
+      ...signe,ans
+    ])
+  }
+  const apiCalcul = (calculToSend) =>{
+    //se connect a l'api de calcul, envoi le string et recupere la reponse de l'api
+    const url='http://127.0.0.1:5000/calc?calc='+encodeURIComponent(calculToSend)
+
+    axios.get(url)
+    .then(objectData=>{
+      const resultat = objectData.data.resultat
+      const calcul = objectData.data.calcul
+      //met le resultat et le calcul dans les hooks
+      setLastCalc([
+        calcul
+      ])
+      setSigne([
+        resultat
+      ])
+      setAns([
+        resultat
+      ])
+    })
+    .catch(err=>{
+      setSigne([
+      "error"
+    ])}
+    )
+  }
+
   return (
     <div>
       <Col xs={{size:2, offset:1}}>
         <Col className="noPadding">
+        <Input value={lastCalc.join("")} disabled block/>
           <Input value={signe.join("")} disabled block/>
         </Col>
           <Row className="noMargin">
@@ -60,10 +101,10 @@ export default (props) => {
           </Row>
           <Row className="noMargin">
             <Col className="noPadding">
-                <Button color="dark" value="Ans"  btnSize="lg" onClick={()=>{console.log("ans")}}/>
+                <Button color="dark" value="Ans"  btnSize="lg" onClick={ansClick}/>
             </Col>
             <Col className="noPadding">
-                <Button color="primary" value="="  btnSize="lg" onClick={()=>{console.log("equal")}}/>
+                <Button color="primary" value="="  btnSize="lg" onClick={equalClick}/>
             </Col>
           </Row>
         </Col>
